38f49ac45cb2aeb97b1f0f94f541eaaf1591cfc9
ge79kis <ge79kis@mytum.de>
2024-02-29 23:54:24 +0100

    added carousel order

--- src/components/Passions.jsx
+++ src/components/Passions.jsx
@@ -24,77 +24,96 @@ function Passions() {
       <TypingEffect text="My Passions"></TypingEffect>
       <div className="carousel-wrapper">
         <div className="carousel-container">
-        <Carousel
-  showArrows={true}
-  showThumbs={true}
-  width={550}
-  transitionTime={1000}
-  interval={5000}
-  showIndicators={true}
-  centerMode={false}
-  infiniteLoop={true}
-  autoPlay={true}
-  axis="horizontal"
-  dynamicHeight={true}
-  centerSlidePercentage={80}
-  autoFocus = {true}
-  showStatus = {false}
-  animationHandler="fade"
->
-  <div>
-    <img src={img_phong_chess_thinking} alt="Chess Thinking" />
-    <p className="legend">Thinking about the next moves ♚</p>
-  </div>
-  <div>
-    <img src={img_phong_run} alt="Running" />
-    <p className="legend">Striving for even better runs 🏃🏼‍♂️</p>
-  </div>
-  <div>
-    <img src={img_fussball_2018} alt="Playing Football" />
-    <p className="legend">Soccer has always been a passion of mine ⚽</p>
-  </div>
-  <div>
-    <img src={img_gartenhaus} alt="Gartenhaus" />
-    <p className="legend">Every now and then it gets a bit more mechanical 🏠</p>
-  </div>
-  <div>
-    <img src={img_vietnam} alt="Vietnam" />
-    <p className="legend">My Asian roots lie in beautiful Vietnam 🇻🇳</p>
-  </div>
-  <div>
-    <img src={img_internationals} alt="International Experiences" />
-    <p className="legend">Getting to know people from other cultures inspires me 🌍</p>
-  </div>
-  <div>
-    <img src={img_phong_greek_flag} alt="Greek Flag" />
-    <p className="legend">Living abroad, like in Greece, are particularly special for me 🇬🇷</p>
-  </div>
-  <div>
-    <img src={img_phong_gym} alt="Gym Workout" />
-    <p className="legend">Being fit and staying fit is part of everyday life 💪</p>
-  </div>
-  <div>
-    <img src={img_phong_science} alt="Science in Prague" />
-    <p className="legend">The strong will to always learn new things and grow 📚</p>
-  </div>
-  <div>
-    <img src={img_phong_tum_graduate} alt="Graduation from TUM" />
-    <p className="legend">The breathtaking feeling of having achieved something and then continuing to grow beyond it 🎓</p>
-  </div>
-  <div>
-    <img src={img_foodtruck} alt="Foodtruck" />
-    <p className="legend">Food is one of my passions, so I worked in a burger food truck 🍔</p>
-  </div>
-  <div>
-    <img src={img_thaiboxen} alt="Thai Boxing" />
-    <p className="legend">Martial arts gives me focus and adrenaline 🥊</p>
-  </div>
-  <div>
-    <img src={img_insi_singen} alt="Insi Singen" />
-    <p className="legend">Contributing to society fulfills me 🌟</p>
-  </div>
-</Carousel>
+          <Carousel
+            showArrows={true}
+            showThumbs={true}
+            width={550}
+            transitionTime={1000}
+            interval={5000}
+            showIndicators={true}
+            centerMode={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            axis="horizontal"
+            dynamicHeight={true}
+            centerSlidePercentage={80}
+            autoFocus={true}
+            showStatus={false}
+            animationHandler="fade"
+          >
+            <div>
+              <img src={img_fussball_2018} alt="Playing Football" />
+              <p className="legend">
+                Soccer has always been a passion of mine ⚽
+              </p>
+            </div>
+            <div>
+              <img src={img_phong_run} alt="Running" />
+              <p className="legend">Striving for even better runs 🏃🏼‍♂️</p>
+            </div>
+            <div>
+              <img src={img_thaiboxen} alt="Thai Boxing" />
+              <p className="legend">
+                Martial arts gives me focus and adrenaline 🥊
+              </p>
+            </div>
+            <div>
+              <img src={img_phong_gym} alt="Gym Workout" />
+              <p className="legend">
+                Being fit and staying fit is part of everyday life 💪
+              </p>
+            </div>
+            <div>
+              <img src={img_phong_chess_thinking} alt="Chess Thinking" />
+              <p className="legend">Thinking about the next moves ♚</p>
+            </div>
+            <div>
+              <img src={img_phong_science} alt="Science in Prague" />
+              <p className="legend">
+                The strong will to always learn new things and grow 📚
+              </p>
+            </div>
+            <div>
+              <img src={img_phong_tum_graduate} alt="Graduation from TUM" />
+              <p className="legend">
+                The breathtaking feeling of having achieved something and then
+                continuing to grow beyond it 🎓
+              </p>
+            </div>
+            <div>
+              <img src={img_phong_greek_flag} alt="Greek Flag" />
+              <p className="legend">
+                Living abroad, like in Greece, are particularly special for me
+                🇬🇷
+              </p>
+            </div>
+            <div>
+              <img src={img_vietnam} alt="Vietnam" />
+              <p className="legend">
+                My Asian roots lie in beautiful Vietnam 🇻🇳
+              </p>
+            </div>
+            <div>
+              <img src={img_internationals} alt="International Experiences" />
+              <p className="legend">
+                Getting to know people from other cultures inspires me 🌍
+              </p>
+            </div>
+            <div>
+              <img src={img_insi_singen} alt="Insi Singen" />
+              <p className="legend">
+                Helping young refugees, contributing to society fulfills me 🌟
+              </p>
+            </div>
 
+            <div>
+              <img src={img_foodtruck} alt="Foodtruck" />
+              <p className="legend">
+                Food is one of my passions, so I worked in a burger food truck
+                🍔
+              </p>
+            </div>
+          </Carousel>
         </div>
       </div>
     </div>
@@ -102,3 +121,18 @@ function Passions() {
 }
 
 export default Passions;
+
+/*
+Fußball
+Running
+Kampfsport
+Gym
+Schach
+Prag
+TUM Abschluss
+Reisen in Griechenland
+Interkulturelles
+Vietnam (Familie)
+InSi Flüchtlingshilfe
+Foodtruck
+*/
