612122590b25544b3851198cd6434a3696d463ff
ge79kis <ge79kis@mytum.de>
2024-02-29 01:40:25 +0100

    improved project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,11 @@ import img_led_matrix from "../assets/led_matrix.png";
 import img_xmc4500 from "../assets/xmc4500.png";
 import img_xai_app from "../assets/xai_app.png";
 
+import img_cpp from "../assets/cpp.png";
+import img_python from "../assets/python.png";
+import img_c from "../assets/c.png";
+import img_react from "../assets/react.png";
+
 import TypingEffect from "./TypingEffect";
 
 function Projects() {
@@ -16,40 +21,40 @@ function Projects() {
       <div className="card-container">
         <ProductCard
           productName="Expense Tracker"
-          photos={[img_splitwise]}
-          description="Terminal-based Splitwise Clone - made in the TUM course Advanced Programming"
+          photos={[img_splitwise, img_cpp]}
+          description="Terminal-based Splitwise Clone - written in C++"
           url="https://github.com/G-Phong/Expense-Tracker"
           buttonText="GitHub Repo"
         ></ProductCard>
 
         <ProductCard
-          productName="Web App: Explainable AI"
-          photos={[img_xai_app]}
-          description="Master thesis TUM-fml 2023: XAI Model for a CP-based Shift Scheduling Algorithm"
+          productName="XAI Web App"
+          photos={[img_xai_app, img_python, img_react]}
+          description="Master thesis 2023: Explainable AI for Preference-based Shift Scheduling - Python Flask and React"
           url="https://github.com/G-Phong/XAI-scheduling-model"
           buttonText="GitHub Repo"
         ></ProductCard>
 
         <ProductCard
           productName="LED Matrix Control"
-          photos={[img_led_matrix]}
-          description="Programming a LED matrix within the TUM project UNICARagil"
+          photos={[img_led_matrix, img_c]}
+          description="Programming a LED matrix for an autonomous vehicle - C++ and a ROS-like Framework"
           url="https://github.com/G-Phong/SA-LED-matrix-UNICARagil"
           buttonText="GitHub Repo"
         ></ProductCard>
 
         <ProductCard
-          productName="Real-Time Microcontroller Programming"
-          photos={[img_beaglebone]}
-          description="Programming of a BeagleBone microcontroller within the TUM course Echtzeitfähige Geräte und Roboter"
+          productName="Real-Time Programming"
+          photos={[img_beaglebone, img_c]}
+          description="Programming of a BeagleBone microcontroller - low-level coding with C"
           url="https://github.com/G-Phong/EGR-microcontroller"
           buttonText="GitHub Repo"
         ></ProductCard>
 
         <ProductCard
-          productName="Embedded Systems & Cybersecurity"
-          photos={[img_xmc4500]}
-          description="Exercises within the TUM course Embedded Systems and Security"
+          productName="Embedded Security"
+          photos={[img_xmc4500, img_c]}
+          description="Exploiting vulnerabilities on a microcontroller - lowe-level coding with C"
           url="https://github.com/G-Phong/ESS-Cybersecurity"
           buttonText="GitHub Repo"
         ></ProductCard>
