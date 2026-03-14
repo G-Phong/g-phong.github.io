1186009b9d76c02d7e01163ee6beea47176dc86b
Gia-Phong Tran <gia-phong.tran.external@tkelevator.com>
2026-03-14 14:44:30 +0100

    Improve header: circular avatar left of name, cleaner layout, updated subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,6 @@ function Header() {
   return (
     <div>
       <div className="header-container">
-        <div>
-          <h1 className="title">Gia-Phong Tran</h1>
-        </div>
-        {/* Circular testimonial section */}
         <div className="testimonial-container">
           <img
             src={img_phong_portrait}
@@ -19,11 +15,11 @@ function Header() {
             className="testimonial-image"
           />
         </div>
+        <h1 className="title">Gia-Phong Tran</h1>
       </div>
       <div className="subtitle-container">
         <h2 className="subtitle">
-          Mechatronics Engineer | Embedded Systems | Multilingual | Sports and
-          Chess Enthusiast
+          Embedded Linux Engineer | C++ & IoT | Marathon Runner | Chess Enthusiast
         </h2>
       </div>
     </div>
